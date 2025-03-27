2b3a136217b3de567863d83cd46348318087cc37
Anis Mahat <anismahat756.am1@gmail.com>
2025-03-27 16:54:07 +0530

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 function Login() {
-  return <div>
-
-    <h1>Hello All</h1>
-    <h2>This is Login Page</h2>
-    <h1>Hello From Pranav</h1>
-
-    <h1>Hello All</h1>
-    <h2>This is Login Page</h2>
-    <h1>Hello From Pranav</h1>
-  </div>;
+  return (
+    <div>
+      <h1>Hello All</h1>
+      <h2>This is Login Page</h2>
+      <h1>Hello From Pranav</h1>
+      <h1>Hello From Anis</h1>
+      <h1>Hello From Sunil</h1>
+    </div>
+  );
 }
 
 export default Login;
