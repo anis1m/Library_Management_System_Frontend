d936bdb3a05989f8540b663b4bce00680530dfb2
Anis Mahat <anismahat756.am1@gmail.com>
2025-04-01 12:12:04 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,24 @@ import HomePage from "./Components/HomePage";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Components/user/Login";
 import Register from "./Components/user/Register";
+import UserDashboard from "./Components/user/userDashboard";
+import AdminDashboard from "./Components/admin/AdminDashboard";
 
 function App() {
-
-
   return (
     <PrimeReactProvider>
       <Routes>
-        <Route path="/" element={<HomePage/>}>
-        <Route  index element={<Login/>}/>
-        <Route path="register" element={<Register/>}/>
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/user" element={<UserDashboard />}>
+          <Route path="books" />
+        </Route>
+        <Route path="/admin" element={<AdminDashboard />}>
+          <Route path="books" />
         </Route>
       </Routes>
-      
     </PrimeReactProvider>
   );
 }
---
+++ src/Components/user/UserDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserDashboard() {
+  return <div>userDashboard</div>;
+}
+
+export default UserDashboard;
